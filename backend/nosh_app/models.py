97bf6f78ed42cca60f0e90cdc48c5423a29f0406
Zack Hanley <110628956+zaHanley@users.noreply.github.com>
2023-02-14 09:42:57 -0600

    Update models.py

--- backend/nosh_app/models.py
+++ backend/nosh_app/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.contrib.auth.models import (AbstractUser)
 from django.contrib.auth import get_user_model
 
-# User = get_user_model()
 
-# Create your models here.
 class User(AbstractUser):
     email = models.EmailField(
         verbose_name='email address',
